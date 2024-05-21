761cc934ef338ea60af0e96f2803672e5a709601
Adrian Pita <pitabermudez@gmail.com>
2024-05-21 17:23:06 +0200

    Primer mapa enseñado, con importaciones e instalaciones necesarias de mapbox

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG9ucGl0YSIsImEiOiJjbHdnaGF4YzMwMDcyMmlvNzJxdzRwbXZzIn0.LPuDDQwTtu_l68KZ0T_v4g';
 
 @Component({
   selector: 'app-full-screen-page',
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+  }
+
 
 }
