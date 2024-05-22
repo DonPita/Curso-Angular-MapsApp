020b2f8d61c352e8485e4d7facb1de520b4e9f39
Adrian Pita <pitabermudez@gmail.com>
2024-05-22 10:11:38 +0200

    Poniendo a funcionar los botones del zoom y la barrita, para que se hagan efectivos al usarlos

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,6 +9,9 @@ import { Map } from "mapbox-gl";
 
 export class ZoomRangePageComponent implements AfterViewInit {
 
+  public zoom: number = 10;
+  public map?: Map;
+
   @ViewChild('map') divMap?: ElementRef; //Referencia al HTML #map
 
   ngAfterViewInit(): void {
@@ -16,11 +19,47 @@ export class ZoomRangePageComponent implements AfterViewInit {
     //Ajuste de seguridad para que no dea un posible error.
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-7.7167, 43.3333], // starting position [lng, lat] Santaballa
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
+    });
+
+    this.mapListeners();
+  }
+
+  //Unir el zoom al HTML
+  mapListeners() {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    //Esto es para que vuelva siempre al zoom 18 si te pasas de rosca.
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+      this.zoom = this.map!.getZoom();
     });
   }
+
+  //Botones
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  //Barra de zoom
+  zoomChanged(value: string) {
+    this.zoom = Number(value); //Transformar el string a number
+    this.map?.zoomTo(this.zoom); //Se manda al mapa el zoom recibido al mover la barrita.
+  }
+
 }
+
