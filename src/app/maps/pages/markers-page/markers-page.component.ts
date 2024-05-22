37bd1c2f70e606a72a6e72ac4fcc5eb9f3517f4e
Adrian Pita <pitabermudez@gmail.com>
2024-05-22 12:36:57 +0200

    Añadiendo marcadores al mapa, no se guardan todavia

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { LngLat, Map, Marker } from "mapbox-gl";
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-7.7167, 43.3333);
+
+  @ViewChild('map') divMap?: ElementRef; //Referencia al HTML #map
+
+  ngAfterViewInit(): void {
+
+    //Ajuste de seguridad para que no dea un posible error.
+    if (!this.divMap) {
+      throw 'El elemento HTML no fue encontrado';
+    }
+
+    this.map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat] Santaballa
+      zoom: this.zoom, // starting zoom
+    });
+    // Marcador en la posicion actual central del mapa
+    // const marker = new Marker()
+    // .setLngLat(this.currentLngLat)
+    // .addTo(this.map);
+  }
+
+  //Metodo para usar con el addMarker y darle los valores necesarios.
+  createMarker() {
+    if (!this.map) {
+      return;
+    }
+
+    const color = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    const lngLat = this.map?.getCenter();
+
+    this.addMarker(lngLat, color);
+  }
+
+  //Añadir Marcador
+  addMarker(lngLat: LngLat, color: string) {
+    if (!this.map) {
+      return;
+    }
+
+    const marker = new Marker({
+      color: color,
+      draggable: true //Indica que se puede arrastrar el marcador
+    })
+      .setLngLat(lngLat)
+      .addTo(this.map);
+
+
+  }
 
 }
