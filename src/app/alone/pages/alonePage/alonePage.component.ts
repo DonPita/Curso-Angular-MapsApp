2656d2aa25421c8a7093d70619e973f24b8cbf48
Adrian Pita <pitabermudez@gmail.com>
2024-05-23 08:57:06 +0200

    Standalone component como pagina

---
+++ src/app/alone/pages/alonePage/alonePage.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alone-page',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alonePage.component.css',
+
+})
+
+export class AlonePageComponent { }
