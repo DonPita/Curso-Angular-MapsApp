0101a87a15384c2ad85c216fd9b21e5a4535ac71
Adrian Pita <pitabermudez@gmail.com>
2024-05-23 09:18:49 +0200

    Standalone Component reutilizable y su conexion

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   selector: 'app-alone-page',
   standalone: true,
   imports: [
     CommonModule,
+    CounterAloneComponent
   ],
   templateUrl: './alone-page.component.html',
-  styleUrl: './alonePage.component.css',
+  styleUrl: './alone-page.component.css',
 
 })
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: 'alone',
-    loadComponent: () => import('./alone/pages/alonePage/alonePage.component').then(m => m.AlonePageComponent),
+    loadComponent: () => import('./alone/pages/alone-page/alone-page.component').then(m => m.AlonePageComponent),
   },
   {
     path: '**',
